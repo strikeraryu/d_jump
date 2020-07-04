f66afc4ab38c7b920a1f79fc4b937ffa81cab7e1
strikeraryu <striker.aryu56@gmail.com>
2020-07-04 12:18:11 +0530

    basic game

--- game_jump.py
+++ game_jump.py
@@ -6,13 +6,33 @@ pg.init()
 
 display_width = 600
 display_height = 400
+jump_vel = 40
+crate_vel = 30
+g = 10
+max_crate = 3
+obj_y = 300
 
-gameDisplay = pg.display.set_mode((display_width,display_height))
+digit = [pg.image.load('images/digit/0.png'), pg.image.load('images/digit/1.png'),
+         pg.image.load('images/digit/2.png'), pg.image.load('images/digit/3.png'),
+         pg.image.load('images/digit/4.png'), pg.image.load('images/digit/5.png'),
+         pg.image.load('images/digit/6.png'), pg.image.load('images/digit/7.png'),
+         pg.image.load('images/digit/8.png'), pg.image.load('images/digit/9.png')]
 
+plant_jump = [pg.image.load('images/plant_jump/plant_jump (1).png'),pg.image.load('images/plant_jump/plant_jump (2).png'),pg.image.load('images/plant_jump/plant_jump (3).png'),pg.image.load('images/plant_jump/plant_jump (4).png')]
+plant_jump = [pg.transform.scale(img,(64,68)) for img in plant_jump]
+plant_run = [pg.image.load('images/plant_run/plant_run (1).png'),pg.image.load('images/plant_run/plant_run (2).png'),pg.image.load('images/plant_run/plant_run (3).png'),pg.image.load('images/plant_run/plant_run (4).png')]
+plant_run = [pg.transform.scale(img,(64,68)) for img in plant_run]
+crate_img = pg.image.load('images/crate.png')
+crate_img = pg.transform.scale(crate_img,(40,45))
+
+
+win = pg.display.set_mode((display_width,display_height))
 pg.display.set_caption("aryamaan chutiya")
+clock = pg.time.Clock()
+
 
 def distance(cord1, cord2):
-    return ((cord1[0]-cord2[0])*2+(cord1[1]-cord2[1])2)*0.5
+    return ((cord1[0]-cord2[0])**2+(cord1[1]-cord2[1])**2)**0.5
 
 def collide(img, img1, x, y, x1, y1):
     player_mask = pg.mask.from_surface(img)
@@ -57,9 +77,94 @@ def color_chng(img, col1, col2):
 
     return img
 
-digit = [pg.image.load('images/digit/0.png'), pg.image.load('images/digit/1.png'),
-         pg.image.load('images/digit/2.png'), pg.image.load('images/digit/3.png'),
-         pg.image.load('images/digit/4.png'), pg.image.load('images/digit/5.png'),
-         pg.image.load('images/digit/6.png'), pg.image.load('images/digit/7.png'),
-         pg.image.load('images/digit/8.png'), pg.image.load('images/digit/9.png')]
 
+#------------------------
+
+class plant(object):
+    img = plant_run[0]
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.wlk_cnt = 0
+        self.jump = False
+        self.vel_y = 0
+
+
+    def move(self):
+        if self.jump==True:
+            self.y -= self.vel_y
+            self.vel_y-=g
+            if self.vel_y < -jump_vel:
+                self.jump = False
+                self.jump_vel = 0
+
+    def draw(self):
+        if not self.jump:
+            self.img = plant_run[self.wlk_cnt]
+            win.blit(self.img,(self.x,self.y))
+            self.wlk_cnt+=1
+            if self.wlk_cnt>=4: self.wlk_cnt = 0
+        else: 
+            self.img = plant_run[0]
+            win.blit(self.img,(self.x,self.y))
+            self.wlk_cnt = 0
+
+
+class crate_obj(object):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def move(self):
+        self.x -= crate_vel
+
+    def draw(self):
+        win.blit(crate_img,(self.x,self.y))
+
+
+
+def redrawgamewindow():
+    win.fill((0,0,0))
+    plant.draw()
+    for crate in crates:
+        crate.draw()
+    pg.display.update()
+
+run = True
+plant = plant(100,obj_y)
+crates = []
+
+while run:
+    clock.tick(14)
+    
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            quit()
+    
+    keys = pg.key.get_pressed()
+
+    if keys[pg.K_UP] and not plant.jump:
+        plant.jump = True
+        plant.vel_y = jump_vel
+
+    if len(crates) <= max_crate:
+        if len(crates)!=0:
+            new_crate = crate_obj(crates[-1].x + random.randint(300,400),obj_y+20)
+            crates.append(new_crate)
+        if len(crates)==0:
+            new_crate = crate_obj(700,obj_y+20)
+            crates.append(new_crate)
+
+    for crate in crates:
+        if crate.x < 0:
+            crates.pop(crates.index(crate))
+        if collide(plant.img,crate_img,plant.x,plant.y,crate.x,crate.y):
+            quit()
+        crate.move()
+    
+
+    plant.move()
+
+    redrawgamewindow()
+        
